8fa077d1f2a7ba36153486fcee230e78c404da1d
박진호 <pjh3@paytalab.com>
2025-12-08 23:50:41 +0900

    `NET 3.5 호환성을 위한 Tuple 및 ValueTuple 변환 확장 메서드 추가`

---
+++ src/Jinobald.Polifill/System/ArrayEx.cs
@@ -0,0 +1,34 @@
+﻿#if NET35
+using System.Runtime.CompilerServices;
+
+namespace System;
+
+public static class ArrayEx
+{
+    private static readonly CacheDict<Type, Array> _emptyArrays = new(256);
+
+    [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+    public static void Fill<T>(T[] array, T value)
+    {
+        if (array == null) throw new ArgumentNullException(nameof(array));
+
+        for (var index = 0; index < array.Length; index++) array[index] = value;
+    }
+
+    extension(Array)
+    {
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static T[] Empty<T>()
+        {
+            var type = typeof(T);
+            if (type == typeof(Type)) return (T[])(object)TypeEx.EmptyTypes;
+
+            if (_emptyArrays.TryGetValue(type, out var array)) return (T[])array;
+
+            var result = new T[0];
+            _emptyArrays[type] = result;
+            return result;
+        }
+    }
+}
+#endif
