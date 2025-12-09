90fcae3b3539202cb3a8414c4a3819db34f16e13
박진호 <pjh3@paytalab.com>
2025-12-09 14:19:36 +0900

    `NET35 지원 종료를 위해 Polyfill 관련 확장 메서드 및 속성 추가, 불필요한 조건부 의존성 제거`

---
+++ src/Jinobald.Polifill/XElementEx.cs
@@ -0,0 +1,44 @@
+#pragma warning disable
+
+namespace System.Xml.Linq;
+
+internal static partial class Polyfill
+{
+    extension(XElement)
+    {
+        /// <summary>
+        ///     Asynchronously creates a new XElement and initializes its underlying XML tree using the specified stream,
+        ///     optionally preserving white space.
+        /// </summary>
+        //Link: https://learn.microsoft.com/en-us/dotnet/api/system.xml.linq.xelement.loadasync?view=net-10.0#system-xml-linq-xelement-loadasync(system-io-stream-system-xml-linq-loadoptions-system-threading-cancellationtoken)
+        public static Task<XElement> LoadAsync(Stream stream, LoadOptions options, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return Task.FromResult(XElement.Load(stream, options));
+        }
+
+        /// <summary>
+        ///     Asynchronously creates a new XElement and initializes its underlying XML tree using the specified text reader,
+        ///     optionally preserving white space.
+        /// </summary>
+        //Link: https://learn.microsoft.com/en-us/dotnet/api/system.xml.linq.xelement.loadasync?view=net-10.0#system-xml-linq-xelement-loadasync(system-io-textreader-system-xml-linq-loadoptions-system-threading-cancellationtoken)
+        public static Task<XElement> LoadAsync(TextReader textReader, LoadOptions options,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return Task.FromResult(XElement.Load(textReader, options));
+        }
+
+        /// <summary>
+        ///     Asynchronously creates a new XElement and initializes its underlying XML tree using the specified text reader,
+        ///     optionally preserving white space.
+        /// </summary>
+        //Link: https://learn.microsoft.com/en-us/dotnet/api/system.xml.linq.xelement.loadasync?view=net-10.0#system-xml-linq-xelement-loadasync(system-xml-xmlreader-system-xml-linq-loadoptions-system-threading-cancellationtoken)
+        public static Task<XElement> LoadAsync(XmlReader reader, LoadOptions options,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return Task.FromResult(XElement.Load(reader, options));
+        }
+    }
+}
---
+++ src/Jinobald.Polifill/ZipArchiveEntryEx.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+#pragma warning disable
+
+namespace System.IO.Compression;
+
+[ExcludeFromCodeCoverage]
+[DebuggerNonUserCode]
+public static partial class ZipArchiveEntryEx
+{
+    extension(ZipArchiveEntry target)
+    {
+#if !NET10
+        /// <summary>
+        ///     OS and application specific file attributes.
+        /// </summary>
+        //Link: https://learn.microsoft.com/en-us/dotnet/api/system.io.compression.ziparchiveentry.externalattributes?view=net-10.0
+        public int ExternalAttributes
+        {
+            get => 0;
+            set { }
+        }
+#endif
+    }
+}
